195366004e73c6cf8b900c5bc78dd3db39311786
Elena Parchevskaya <elena.lemoon@gmail.com>
2024-03-12 14:50:22 +0100

    extra tag removed

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           Welcome to Women in Tech course!
-        </p>
       </div>
     </main>
   );
